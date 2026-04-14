f21be3091198de24615fed4ef1ebc368cfff8da9
Lenar Shaidullin <l.shaidullin@group-ib.com>
2026-04-14 19:56:47 +0300

    $@

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 import CustomLogger from './src/integrations/custom-logger';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    CustomLogger(),
-  ],
+  site: 'https://yourdomain.com', // prod domain
+  integrations: [CustomLogger()],
 });
--- src/data/data.ts
+++
@@ -1,7 +0,0 @@
-export const SEO_DEFAULTS = {
-  siteName: "название сайта",
-  defaultOgImage: "ссылка на картинку",
-  index: false,
-  ogLocale: "ru_RU",
-  twitterCardFallback: "summary"
-};
